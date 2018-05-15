0a8421d7452af89d1fe6e65b3747c653daec2ead
Arthur Rodrigues <arthurodrigues_2010@hotmail.com>
2018-05-15 16:56:56 -0300

    New Update

--- js/data-angular.js
+++ js/data-angular.js
@@ -13,8 +13,23 @@ angular.module("angular-page").controller("teamctrl", function ($scope){
 angular.module("angular-page").controller("servicesctrl", function ($scope){
 
 	$scope.services = [
-		{icon: 'fa fa-shopping-cart',title: 'E-Commerce', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
-		{icon: 'fa fa-laptop', title: 'Responsive Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
-		{icon: 'fa fa-lock', title: 'Web Security', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fab fa-html5',title: 'HTML 5', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fab fa-css3-alt',title: 'CSS 3', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fab fa-angular', title: 'Angular JS', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fas fa-laptop', title: 'Responsive Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+	];
+});
+
+angular.module("angular-page").controller('portfolioctrl', function ($scope){
+
+	$scope.portfolio = [
+		{image: 'images/portfolio/portfolio1.jpg', title: 'Title Portfolio 1'},
+		{image: 'images/portfolio/portfolio2.jpg', title: 'Title Portfolio 2'},
+		{image: 'images/portfolio/portfolio3.jpg', title: 'Title Portfolio 3'},
+		{image: 'images/portfolio/portfolio4.jpg', title: 'Title Portfolio 4'},
+		{image: 'images/portfolio/portfolio5.jpg', title: 'Title Portfolio 5'},
+		{image: 'images/portfolio/portfolio6.jpg', title: 'Title Portfolio 6'},
+		{image: 'images/portfolio/portfolio7.jpg', title: 'Title Portfolio 7'},
+		{image: 'images/portfolio/portfolio8.jpg', title: 'Title Portfolio 8'}
 	];
 });
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-	//$("html").animate({scrollTop: 0}, 1000);
-
+	//ANIMATE SCROLL-PAGE
 	$(window).scroll(function(){
 		if ($(document).scrollTop() > 100) {
 			$('#menu-fixed').css('top','0');
@@ -35,6 +34,13 @@ $(document).ready(function(){
 		}, 1000);
 	});
 
+	$('#down-page').click(function(){
+		$('html, body').animate({
+			scrollTop: $('div#services').offset().top
+		}, 1000);
+	});
+
+	// LINKS NAV
 	$("#link-services").click(function(){
 		//$("html").animate({scrollTop: 800}, 1000);
 		//$(document).scrollTo("#services");
@@ -75,10 +81,51 @@ $(document).ready(function(){
 		$('#menu-slider').removeClass('open');
 	});
 
+	$('#link-portfolio').click(function(){
+		$('html, body').animate({
+			scrollTop: $('div#portfolio').offset().top
+		}, 1000)
+	});
+
+	$('#link-portfolio-fixed').click(function(){
+		$('html, body').animate({
+			scrollTop: $('div#portfolio').offset().top
+		}, 1000);
+	});
+
+	$('#link-portfolio-mobile').click(function(){
+		$('html, body').animate({
+			scrollTop: $('div#portfolio').offset().top
+		}, 1000);
+		$('#menu-slider').removeClass('open');
+	});
+
+	//ANIMATE SPAN SERVICES
 	$("#services span").on("mouseover" , function(){
-		// $("#services .icones span").addClass("estilomouse");
 		$(this).css("background-color","#e3b928");
 	}).on("mouseout", function(){
 		$(this).css("background-color","#fed136");
 	});
+
+	//OPEN BOX IMAGE
+	$('#portfolio .images-portfolio img').click(function(){
+		var image = $(this).attr('src');
+		var title = $(this).attr('alt');
+
+		$('#portfolio').addClass('open-box-image');
+		$('#image-open img').attr({'src':image});
+	});
+
+	$('#image-open .close-box-image').click(function(){
+		$('#portfolio').removeClass('open-box-image');
+	});
+
+	$('#image-open .close-box-image').click(function(){
+		$('#portfolio').removeClass('open-box-image');
+	});
+
+	$('#portfolio .mask-image-open').click(function(){
+		$('#portfolio').removeClass('open-box-image');
+	});
+
 })
