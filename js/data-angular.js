3a81d058b146783815b39478bf1fcbc9d904aa4a
Arthur Rodrigues <arthurodrigues_2010@hotmail.com>
2018-05-15 09:36:44 -0300

    New Update

--- js/data-angular.js
+++ js/data-angular.js
@@ -8,5 +8,13 @@ angular.module("angular-page").controller("teamctrl", function ($scope){
 		{image: 'images/team3.jpg', name: 'Debora Nunes', job: 'Marketing'},
 		{image: 'images/team4.jpg', name: 'Gabriela Castro', job: 'Developer'}
 	];
+});
 
-})
+angular.module("angular-page").controller("servicesctrl", function ($scope){
+
+	$scope.services = [
+		{icon: 'fa fa-shopping-cart',title: 'E-Commerce', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fa fa-laptop', title: 'Responsive Design', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+		{icon: 'fa fa-lock', title: 'Web Security', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+	];
+});
